cf7b05cfc6d507cd70d9aef1f136f45e61edb9f1
rajaramkuberan <rajaram.kuberan@gmail.com>
2024-03-05 12:04:00 +0000

    uploaded parsed sample data

--- AI_Projects/Computer_Vision/AI_Sensor_Fusion_Project/mmWaveRadar/radar_processing/dataframe_creation.py
+++ AI_Projects/Computer_Vision/AI_Sensor_Fusion_Project/mmWaveRadar/radar_processing/dataframe_creation.py
@@ -8,10 +8,10 @@ import glob
 def dataframe_creation():
 
     try:
-        df1 = pd.read_csv(r"data_20240224-113829.csv")
+        df1 = pd.read_csv(r"./parsed_data/data_20240224-231830.csv")
 
         # Read the second CSV file
-        df2 = pd.read_csv(r"data_20240224-114329.csv")  
+        df2 = pd.read_csv(r"./parsed_data/data_20240224-232330.csv")  
         
         # Concatenate the two dataframes
         #df = pd.concat([df1, df2], ignore_index=True)
@@ -21,4 +21,3 @@ def dataframe_creation():
         
     except Exception as e:
         print(f"An error occurred: {e}")
-
